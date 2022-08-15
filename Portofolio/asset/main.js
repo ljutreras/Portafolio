47a64a50052b50b0980fe7bd70e9b0a93fdea4e3
Leonardo Utreras <ljutreras96@gmail.com>
2022-08-15 15:43:33 -0400

    Agregando carpetas de portafolio

---
+++ Portofolio/asset/main.js
@@ -0,0 +1,6 @@
+const menuLateral = document.querySelector("#nav-menu");
+const inicio = document.querySelector("#inicio");
+const portafolio = document.querySelector("#portafolio");
+const resumen = document.querySelector("#resumen");
+const contacto = document.querySelector("#contacto");
+
